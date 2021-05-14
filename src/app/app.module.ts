477bd739258056d488ad9b289ee50a8db392e29d
Akshath_Pandey <akshathpandey4266@gmail.com>
2021-05-14 16:20:50 +0530

    routing and navigation implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
 import { ProductsComponent } from './products/products.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { ContactsComponent } from './contactus/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductsComponent,
     ProductDetailsComponent,
+    HomeComponent,
+    RegisterComponent,
+    LoginComponent,
+    ContactsComponent,
     
   ],
   imports: [
