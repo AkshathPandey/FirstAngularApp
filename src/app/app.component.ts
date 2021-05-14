54d0e1177438e3e3f1f9dc5a2e0e9289245f2a55
Akshath_Pandey <akshathpandey4266@gmail.com>
2021-05-14 13:56:23 +0530

    components architecture and interaction

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+//    Names:string[]=[];
+//    status:boolean=true;
+//  changStatus(){
+//      this.status=!this.status;
+//    }
+//    sendName(n){
+//      this.Names.push(n);
+//    }
+  // users=[];
+  // addUserData(ref){
+  //   let userObj=ref.value;
+  //   console.log(userObj)
+  //   ref.result;
+  //   this.users.push(userObj);
+  // }
+  // deleteItem(index){
+  //   this.users.splice(index,1);
+  // }
+  
+}
